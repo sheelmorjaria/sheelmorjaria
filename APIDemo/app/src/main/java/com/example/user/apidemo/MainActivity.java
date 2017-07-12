ee63e7a59f0acd6509fcaabc90a1982df1d1d780
Sheel Morjaria <sheelmorjaria@gmail.com>
2017-07-12 14:13:10 +0100

    added swipe to refresh

--- APIDemo/app/src/main/java/com/example/user/apidemo/MainActivity.java
+++ APIDemo/app/src/main/java/com/example/user/apidemo/MainActivity.java
@@ -1,37 +1,71 @@
 package com.example.user.apidemo;
 
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
 
 import com.example.user.apidemo.adapters.CakesAdapter;
 import com.example.user.apidemo.model.CakesModel;
 import com.example.user.apidemo.service.ConnectionService;
 import com.example.user.apidemo.service.RequestInterface;
+import com.github.pwittchen.reactivenetwork.library.rx2.ReactiveNetwork;
 
 import java.util.List;
 
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.functions.Consumer;
 import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 private RequestInterface requestInterface;
     RecyclerView myRecyclerView;
     CakesAdapter cakeAdapter;
+    private SwipeRefreshLayout swipeRefreshLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recyclerview);
-        requestInterface= ConnectionService.getConnection();
-        requestInterface.getCakelist()
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeOn(Schedulers.newThread())
-                .subscribe(this::onSuccess, this::onError);
+        requestInterface = ConnectionService.getConnection();
+
+        swipeRefreshLayout=(SwipeRefreshLayout)findViewById(R.id.swipeRefreshCakes);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                updateOperation();
+            }
+        });
+
     }
+
+    public void updateOperation(){
+        ReactiveNetwork.observeInternetConnectivity()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<Boolean>(){
+                    @Override public void accept(@NonNull Boolean isConnectedToInternet) {
+                        if(isConnectedToInternet){
+                            // do something with isConnectedToInternet value
+                            requestInterface.getCakelist()
+                                    .observeOn(AndroidSchedulers.mainThread())
+                                    .subscribeOn(Schedulers.newThread())
+                                    .subscribe(MainActivity.this::onSuccess,MainActivity.this::onError);
+                            Toast.makeText(MainActivity.this,"network connected",Toast.LENGTH_LONG).show();
+                        }
+                        else{
+                            Toast.makeText(MainActivity.this, "network unavailable",Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
 
     private void onSuccess(List<CakesModel> cakesModels) {
         //call recyclerview
+        //if no network save api data to realm
             cakeAdapter =new CakesAdapter(cakesModels, getApplicationContext());
             myRecyclerView =(RecyclerView)findViewById(R.id.myRecyclerView);
             myRecyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
